71064224af9c84d7bbc571f1abe2c3713aa4e158
Eugenio <eugeniouglov@gmail.com>
2022-09-17 18:22:10 +0200

    First publish
    Check code on correct and wrong and invoke event.

---
+++ Assets/Scripts/DigitalLockView.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DigitalLockView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _outputCodeTMP;
+    [SerializeField] private MonoBehaviour _controller;
+    private INumberButtonClickable _numberButtonClickable;
+    private string _outputCodeString;
+
+
+    private void Awake()
+    {
+        _numberButtonClickable = _controller.GetComponent<INumberButtonClickable>();
+    }
+
+    public void SetCode(int newCode)
+    {
+        _outputCodeTMP.text = newCode.ToString();
+    }
+    
+    public void SetOutputCodeString(string newCodeString)
+    {
+        _outputCodeTMP.text = newCodeString;
+    }
+
+    public string GetOutputCode()
+    {
+        return _outputCodeString;
+    }
+
+    public void OnClickNumberButton(int clickedNumber)
+    {
+        _numberButtonClickable.OnClickNumberButton(clickedNumber);
+    }
+}
---
+++ Assets/Scripts/Interfaces/INumberButtonClickable.cs
@@ -0,0 +1,7 @@
+﻿namespace Interfaces
+{
+    public interface INumberButtonClickable
+    {
+        void OnClickNumberButton(int clickedNumber);
+    }
+}
